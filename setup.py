2a561e440658aafe496596fb26113d30592032c3
wweijtje <85878088+wweijtje@users.noreply.github.com>
2023-07-26 08:41:47 +0200

    Update setup.py
    Removed the parse_requirements function

--- setup.py
+++ setup.py
@@ -22,18 +22,6 @@ with open(init_file, 'r') as f:
 with open('README.rst', 'r') as f:
     readme = f.read()
 
-# Automatically parse the requirements.txt file for project requirements
-def parse_requirements(filename):
-    ''' Load requirements from a pip requirements file '''
-    with open(filename, 'r') as fd:
-        lines = []
-        for line in fd:
-            line.strip()
-            if line and not line.startswith("#"):
-                lines.append(line)
-    return lines
-
-requirements = parse_requirements('requirements.txt')
 
 if __name__ == '__main__':
     setup(
